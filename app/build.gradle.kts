817e3432f9fb7408577613a5685eff47bcaa4cc7
Rosa Merino <rosa.merino@dm-b2b.com>
2025-08-15 12:00:27 +0200

    minSdk changed to 28

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     defaultConfig {
         applicationId = "com.rosamerino.codechallenge"
-        minSdk = 24
+        minSdk = 28
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
